ff4a895f3592e2cd951c8bd1643f97ad15fb6934
amaaren2-sys <amaaren2@gmail.com>
2026-09-17 01:14:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.MA_CONFIG = {
   SUPABASE_URL: 'https://rvnqjgbkwuudbgvjwuoj.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_UXpgYCj9lNuCPIJP5ARgCg_a5wLn8HZ',
 
-  WHATSAPP_NUMBER: '9639XXXXXXXX',
+  WHATSAPP_NUMBER: '963940430278',
   STORE_NAME: 'MA Dental Supplies',
 
   INSTAGRAM_URL: '#',
